1f62f011f71036fa0ab7e9f76e02087366541099
joe-re <joe.tialtngo@gmail.com>
2016-02-10 10:53:26 +0900

    implements incremental search function

--- server/src/assets/javascripts/components/file_list.react.js
+++ server/src/assets/javascripts/components/file_list.react.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 export default class FileList extends React.Component {
+  constructor() {
+    super();
+    this.state = { searchingValue: '' };
+
+  }
   handleSelect(file) {
     this.props.onChange(file);
   }
+  handleChangeIncrementalSearchInput(e) {
+    this.setState({ searchingValue: e.target.value });
+  }
   render() {
     const options = this.props.files.map((file) => {
+      const { searchingValue } = this.state;
+      if (searchingValue && file.name.indexOf(searchingValue) < 0) {
+        return;
+      }
       let className = 'item';
       if (file === this.props.viewingFile) {
         className += ' active';
@@ -23,7 +35,7 @@ export default class FileList extends React.Component {
     return(
       <div className="file-list col-xs-5">
         <h5>{this.props.title}</h5>
-        <input className="incremental-search-input"/>
+        <input className="incremental-search-input" onChange={this.handleChangeIncrementalSearchInput.bind(this)}/>
         <div className="file-select-box">
           {options}
         </div>
